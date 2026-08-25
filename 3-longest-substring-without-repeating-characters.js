948ce7e30f1445ffb84bbe377abb999f948b301d
Nishant Adhikari <165397942+Nishant7Adhikari@users.noreply.github.com>
2026-08-25 19:39:29 +0545

    3 Longest Substring Without Repeating Characters - Runtime: 1503 ms, Memory: 63.5 MB

--- 3-longest-substring-without-repeating-characters.js
+++ 3-longest-substring-without-repeating-characters.js
@@ -3,19 +3,14 @@
  * @return {number}
  */
 var lengthOfLongestSubstring = function(s) {
-    let hash = [];
     let string = [];
-    let pointer = 0;
     let max = 0;
     for (let i = 0; i < s.length; i++) {
-        for (let j = pointer; j < s.length; j++) {
-            if (!string.includes(s[j])) {
+        for (let j = i; j < s.length; j++) {
+            if (!string.includes(s[j]))
                 string.push(s[j]);
-            }
-            else{
-                pointer++;
+            else
                 break;
-            }
         }
         if(string.length > max)
             max = string.length;
